193af9558ff009b7726e19df2ff808fc91b68504
Svetlana <sss041296@mail.ru>
2021-09-11 15:48:04 +0400

    add directive resize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { fromEvent, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  private destroy = new Subject();
-
-  // eslint-disable-next-line
-  ngOnInit(): void {
-    fromEvent(window, 'resize')
-      .pipe(takeUntil(this.destroy))
-      .subscribe(() => {
-        const mvp = document.getElementById('meta-viewport');
-        if (window.screen.width < 320) {
-          mvp?.setAttribute('content', 'user-scalable=no, width=320');
-        } else {
-          mvp?.setAttribute('content', 'width=device-width, initial-scale=1');
-        }
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy.next();
-    this.destroy.complete();
-  }
-}
+export class AppComponent {}
